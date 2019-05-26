e7c61b945b21eb0b156452f40bf1e76c67c1b8a9
AlmakhanovN <almahanov567@gmail.com>
2019-05-26 18:56:24 +0600

    Завершил пункт номер 2

--- source/js/map.js
+++ source/js/map.js
@@ -290,15 +290,23 @@ similarMapPinList.addEventListener('click', onMapPinClick)
 
 var adFormTitleInput = adForm.querySelector('#title');
 
+var setErrorStyle = function(element, thickness, color) {
+    element.style.border = thickness + ' solid ' + color;
+}
+
 adFormTitleInput.addEventListener('invalid', function() {
     if(adFormTitleInput.validity.tooShort) {
         adFormTitleInput.setCustomValidity('Заголовок объявления должен состоять минимум из 30-и символов. Длинна текста сейчас ' + adFormTitleInput.value.length);
+        setErrorStyle(adFormTitleInput, '2px', '#ff0000');
     } else if(adFormTitleInput.validity.tooLong) {
         adFormTitleInput.setCustomValidity('Заголовок объявления должен состоять максимум из 100 символов. Длинна текста сейчас ' + adFormTitleInput.value.length);
+        setErrorStyle(adFormTitleInput, '2px', '#ff0000');
     } else if(adFormTitleInput.validity.valueMissing) {
         adFormTitleInput.setCustomValidity('Обязательное поле');
+        setErrorStyle(adFormTitleInput, '2px', '#ff0000');
     } else {
         adFormTitleInput.setCustomValidity('');
+        setErrorStyle(adFormTitleInput, '1px', "#d9d9d3");
     }
 })
 
@@ -337,12 +345,16 @@ adFormType.addEventListener('change', getMinPrice)
 adFormPriceInput.addEventListener('invalid', function() {
     if(adFormPriceInput.validity.rangeOverflow) {
         adFormPriceInput.setCustomValidity('Цена не должна превышать ' + adFormPriceInput.max + ' рублей');
+        setErrorStyle(adFormPriceInput, '2px', '#ff0000');
     } else if (adFormPriceInput.validity.rangeUnderflow) {
         adFormPriceInput.setCustomValidity('Цена не должна быть ниже ' + adFormPriceInput.min + ' рублей');
+        setErrorStyle(adFormPriceInput, '2px', '#ff0000');
     } else if(adFormPriceInput.validity.valueMissing) {
         adFormPriceInput.setCustomValidity('Обязательное поле');
+        setErrorStyle(adFormPriceInput, '2px', '#ff0000');
     } else {
         adFormPriceInput.setCustomValidity('');
+        setErrorStyle(adFormPriceInput, '1px', "#d9d9d3");
     }
 })
 
@@ -369,18 +381,39 @@ var adFormNumderGuestsSelect = adForm.querySelector('#capacity');
 
 var syncRoomsGuetsSelect = function(evt) {
     var target = evt.currentTarget;
-    var selectedOption = target.options[target.selectedIndex].value;
+    var selectedOption = target.options[target.selectedIndex];
     
-    console.log(adFormNumderGuestsSelect);
     for(var i = 0; i < target.length; i++) {     
-        adFormNumderGuestsSelect.options[i].disabled = false;
-        if(+selectedOption < +adFormNumderGuestsSelect.options[i].value || +adFormNumderGuestsSelect.options[i].value === AD_NOT_GUESTS) {
-            console.log(adFormNumderGuestsSelect.options[i].value);
+        adFormNumderGuestsSelect.options[i].disabled = true;
+        if(+selectedOption.value >=  +adFormNumderGuestsSelect.options[i].value) {
+            adFormNumderGuestsSelect.options[i].disabled = false;
+            if(+adFormNumderGuestsSelect.options[i].value === AD_NOT_GUESTS) {
+                adFormNumderGuestsSelect.options[i].disabled = true;
+            } 
+        } 
+        if(+selectedOption.value === AD_MAX_ROOMS) {
             adFormNumderGuestsSelect.options[i].disabled = true;
-        } else if(+selectedOption === AD_MAX_ROOMS && +adFormNumderGuestsSelect.options[i].value != AD_NOT_GUESTS) {
-            adFormNumderGuestsSelect.options[i].disabled = true;
+            if(+adFormNumderGuestsSelect.options[i].value === AD_NOT_GUESTS) {
+                adFormNumderGuestsSelect.options[i].disabled = false;
+            }
         }
     }
 }
 
-adFormNumberRoomsSelect.addEventListener('change', syncRoomsGuetsSelect)
+
+var validityNumberAndGuestsInputs = function() {
+    if(adFormNumderGuestsSelect.options[adFormNumderGuestsSelect.selectedIndex].disabled === true) {
+        adFormNumderGuestsSelect.setCustomValidity('Выбрано недопустимое значение. Пожалуйста, попробуйте выбрать');
+        setErrorStyle(adFormNumderGuestsSelect, '2px', '#ff0000');;
+    } else {
+        adFormNumderGuestsSelect.setCustomValidity('');
+        setErrorStyle(adFormNumderGuestsSelect, '1px', "#d9d9d3");
+    }
+}
+
+adFormNumberRoomsSelect.addEventListener('change', function(evt) {
+    syncRoomsGuetsSelect(evt);
+    validityNumberAndGuestsInputs();
+})
+
+adFormNumderGuestsSelect.addEventListener('change', validityNumberAndGuestsInputs)
